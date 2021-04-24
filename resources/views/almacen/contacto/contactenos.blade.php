471e9263508eb8f3dd1872a431ec9f1b33ed1e76
MiguelPalaciosC <miguel.palacios@uptc.edu.co>
2021-04-24 10:11:20 -0500

    Update contactenos.blade.php
    Update page contact us (text only)

--- resources/views/almacen/contacto/contactenos.blade.php
+++ resources/views/almacen/contacto/contactenos.blade.php
@@ -1,12 +1,13 @@
 @extends ('layouts.admin')
 @section ('head')
-    <div class="masthead-subheading">Información de la empresa</div>
+    <div class="masthead-subheading">Contáctenos</div>
 @stop
 @section ('contenido')
 
     <!--Info de Contacto -->
     <section class="section section-lg text-center bg-default">
         <div class="container">
+        <h3 class="section-subheading text-muted">Información de la empresa</h3>
           <div class="row row-50">
             @foreach ($empresa as $emp)
             <!-- Telefono -->
@@ -51,35 +52,34 @@
     <section class="page-section" id="contact">
         <div class="container">
             <div class="text-center">
-                <h2 class="section-heading text-uppercase">Contact Us</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <h2 class="section-heading text-uppercase">Buzón de sugerencias</h2>
             </div>
             <form id="contactForm" name="sentMessage" novalidate="novalidate">
                 <div class="row align-items-stretch mb-5">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                            <input class="form-control" id="name" type="text" placeholder="Nombre *" required="required" data-validation-required-message="Please enter your name." />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                            <input class="form-control" id="email" type="email" placeholder="Correo *" required="required" data-validation-required-message="Please enter your email address." />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="form-group mb-md-0">
-                            <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                            <input class="form-control" id="phone" type="tel" placeholder="Telefono *" required="required" data-validation-required-message="Please enter your phone number." />
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group form-group-textarea mb-md-0">
-                            <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                            <textarea class="form-control" id="message" placeholder="Sugerencia o queja *" required="required" data-validation-required-message="Please enter a message."></textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                 </div>
                 <div class="text-center">
                     <div id="success"></div>
-                    <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
+                    <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Enviar mensaje</button>
                 </div>
             </form>
         </div>
