7580d4b63d5e6749d0af10ef83a672cd33e77cb7
MiguelPalaciosC <miguel.palacios@uptc.edu.co>
2021-04-24 11:46:08 -0500

    Icons of services

--- resources/views/almacen/start/index.blade.php
+++ resources/views/almacen/start/index.blade.php
@@ -21,32 +21,39 @@
                 <h4>Prueba se tiene que borrar</h4>
                 <h2 class="section-heading text-uppercase">Servicios</h2>
                 <h3 class="section-subheading text-muted">A continuación se muestran nuestros servicios...</h3>
-            </div>    
+            </div>
+            <!-- servicios -->
             <div class="row text-center">
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="my-3">Mantenimiento de impresoras</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <!--  M. impresora-->
+                <div class="col-md-6 col-lg-4">
+                    <div class="box-icon-classic">
+                    <div class="box-icon-inner decorate-rectangle decorate-color-primary-2"><span class="icon-xl linearicons-printer icon-gradient-1"></span></div>
+                        <div class="box-icon-caption">
+                            <h4 class="my-3">Mantenimiento de impresoras</h4>
+                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="my-3">Mantenimiento de equipos</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <!-- M. equipos -->
+                <div class="col-md-6 col-lg-4">
+                    <div class="box-icon-classic">
+                    <div class="box-icon-inner decorate-triangle decorate-color-secondary"><span class="icon-xl linearicons-desktop icon-gradient-1"></span></div>
+                        <div class="box-icon-caption">
+                            <h4 class="my-3">Mantenimiento de equipos</h4>
+                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="my-3">Actualización de software</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                </div>
+                <!-- Actualizar informacion -->
+                <div class="col-md-6 col-lg-4">
+                    <div class="box-icon-classic">
+                    <div class="box-icon-inner decorate-circle decorate-color-secondary-2"><span class="icon-xl linearicons-folder-download icon-gradient-1"></span></div>
+                        <div class="box-icon-caption">
+                            <h4 class="my-3">Actualización de software</h4>
+                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                        </div>
+                    </div>
+                </div> 
             </div>
         </div>
     </section>
