a9f07dedd960652a5b2f2914cd1f65ae0cf7f9a6
MiguelPalaciosC <miguel.palacios@uptc.edu.co>
2021-05-14 18:28:43 -0500

    add view 'facturacion'
    create controller 'ImprimirFacturaController' and bill for client with device maintenance

---
+++ resources/views/almacen/imprimir/edit.blade.php
@@ -0,0 +1,130 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true"
+role="dialog" tabindex="-1" id="modal-edit-{{$tbj->id_cliente}}">
+
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" 
+					aria-label="Close">
+                     <span aria-hidden="true">×</span>
+                </button>
+				 <h5 class="modal-title">FACTURACIÓN</h5>               
+			</div>
+			<div class="modal-body">
+				<div class="container-fluid">
+					<div id="page_pdf">
+						<table id="factura_head">
+							<tr>
+								<td class="logo_factura">
+									<div>
+										<img src="{{URL::asset('assets/img/logos/jtm (1).png')}}">
+									</div>
+								</td>
+								<td class="info_empresa">
+									<div>
+										<span class="h2">SISTEMA VENTAS ABELOSH</span>
+										<p>Avenida las américas Zona 13, Guatemala</p>
+										<p>Teléfono: +(502) 2222-3333</p>
+										<p>Email: info@abelosh.com</p>
+									</div>
+								</td>
+							</tr>
+						</table>
+						<table id="factura_cliente">
+							<tr>
+								<td class="info_cliente">
+									<div class="round">
+										<span class="h3">Cliente</span>
+										<table class="datos_cliente">
+											<tr>
+												<td><label>Nit:</label><p>54895468</p></td>
+												<td><label>Teléfono:</label> <p>7854526</p></td>
+											</tr>
+											<tr>
+												<td><label>Nombre:</label> <p>Angel Arana Cabrera</p></td>
+												<td><label>Dirección:</label> <p>Calzada Buena Vista</p></td>
+											</tr>
+										</table>
+									</div>
+								</td>
+
+							</tr>
+						</table>
+
+						<table id="factura_detalle">
+								<thead>
+									<tr>
+										<th width="50px">Cant.</th>
+										<th class="textleft">Descripción</th>
+										<th class="textright" width="150px">Precio Unitario.</th>
+										<th class="textright" width="150px"> Precio Total</th>
+									</tr>
+								</thead>
+								<tbody id="detalle_productos">
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+									<tr>
+										<td class="textcenter">1</td>
+										<td>Plancha</td>
+										<td class="textright">516.67</td>
+										<td class="textright">516.67</td>
+									</tr>
+								</tbody>
+								<tfoot id="detalle_totales">
+									<tr>
+										<td colspan="3" class="textright"><span>SUBTOTAL Q.</span></td>
+										<td class="textright"><span>516.67</span></td>
+									</tr>
+									<tr>
+										<td colspan="3" class="textright"><span>IVA (12%)</span></td>
+										<td class="textright"><span>516.67</span></td>
+									</tr>
+									<tr>
+										<td colspan="3" class="textright"><span>TOTAL Q.</span></td>
+										<td class="textright"><span>516.67</span></td>
+									</tr>
+							</tfoot>
+						</table>
+						<div>
+							<p class="nota">Si usted tiene preguntas sobre esta factura, <br>pongase en contacto con nombre, teléfono y Email</p>
+							<h4 class="label_gracias">¡Gracias por su compra!</h4>
+						</div>
+
+					</div>
+				</div>           
+			</div>
+			<div class="modal-footer">
+			</div>
+		</div>
+	</div>
+	
+
+</div>
